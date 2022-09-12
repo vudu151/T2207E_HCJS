3d6f12ad793fc4ebda58ab7f03d5783b4a01a588
vudu151 <109599042+vudu151@users.noreply.github.com>
2022-09-12 19:00:55 +0700

    up code

--- js/demoforecastweather.js
+++ js/demoforecastweather.js
@@ -14,7 +14,7 @@ function forecastAjax(){
             document.getElementById("forecast").innerHTML += f;
         }
     }
-    var url = "http://api.openweathermap.org/data/2.5/forecast?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
+    var url = "https://api.openweathermap.org/data/2.5/forecast?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
     xhttp.open("GET",url,true);
     xhttp.send();
 }
